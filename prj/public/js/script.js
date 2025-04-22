4c7df800a0df270f3d992185d17fc8bafdbce7b2
AncelmoSilva10 <ancelmo10.sousa@gmail.com>
2025-04-22 18:08:53 -0300

    Criando o Projeto

---
+++ prj/public/js/script.js
@@ -0,0 +1,5 @@
+function comecar() {
+    alert("Você clicou em Começar!");
+    // window.location.href = "pagina-inicial.html";
+  }
+  
---
+++ prj/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bem-vindo - CR-Doces</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <img class="logo" src="img/logo-cr-doces.png" alt="Logo CR-Doces">
+  <h1>Bem-vindo à CR-DOCES!</h1>
+  <button onclick="comecar()">Começar</button>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
